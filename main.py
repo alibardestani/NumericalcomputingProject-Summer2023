c67098e2eec13482abaf1087b10741c0f13f1bdf
aliwdxa <alibardestani201796@gmail.com>
2023-07-07 14:21:43 +0330

    first use matplotlib

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the dataset
 data = pd.read_csv('Real estate.csv')
 
-# Display the first few rows of the dataset
-print(data.head())
+# Scatter plot of house age vs house price
+plt.scatter(data['X2 house age'], data['Y house price of unit area'])
+plt.xlabel('House Age')
+plt.ylabel('House Price')
+plt.title('House Age vs House Price')
+plt.show()
